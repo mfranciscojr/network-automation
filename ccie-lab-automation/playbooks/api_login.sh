d4bfe718bae6e4b1c137e56b194cee163edc1cbe
mfranciscojr <mfranciscojr@gmail.com>
2020-02-15 23:55:04 +0800

    change to default credentials

--- ccie-lab-automation/playbooks/api_login.sh
+++ ccie-lab-automation/playbooks/api_login.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-curl -s -b /tmp/cookie -c /tmp/cookie -X POST -d '{"username":"admin","password":"admin123"}' http://127.0.0.1/api/auth/login
+curl -s -b /tmp/cookie -c /tmp/cookie -X POST -d '{"username":"admin","password":"eve"}' http://127.0.0.1/api/auth/login
